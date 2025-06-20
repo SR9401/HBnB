c71b33f45138d24042ae08547ed264fd8316e8f3
Sweetyamnesia <angela.r@live.fr>
2025-06-20 15:55:59 +0200

    ajout d'une vérification et des parenthèses pour self save

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -14,11 +14,12 @@ class Amenity(BaseModel):
     def name(self, value):
         if not isinstance(value, str):
             raise TypeError("Name must be a string")
-    
+        if not value:
+            raise ValueError("Name cannot be empty")
         if len(value) > 50:
             raise ValueError("Name must be < 50 characters")
         self.__name = value
-        self.save
+        self.save()
 
     def to_dict(self):
         return {
