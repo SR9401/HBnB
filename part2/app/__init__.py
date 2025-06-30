6417155eb6491dd985f00945fe5f817a9c376042
Sweetyamnesia <angela.r@live.fr>
2025-06-30 15:01:32 +0200

    Fixe l'init de JWTManager et ajoute le namespace auth

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from flask_jwt_extended import JWTManager
+from app.api.v1.auth import api as auth_ns 
+
+jwt = JWTManager()
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
-    jwt.init_app(app)
     app.config["JWT_SECRET_KEY"] = "super-secret"
-    jwt = JWTManager(app)
-
+    jwt.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
@@ -21,5 +24,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
