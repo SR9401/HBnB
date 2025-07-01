c1469d148f86cdd8e389611d460e2bf8320742f6
Sweetyamnesia <angela.r@live.fr>
2025-07-01 15:16:38 +0200

    mise à jour du fichier init de l'app

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,11 +3,14 @@ from flask_restx import Api
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-
+from app.api.v1.auth import api as auth_ns 
 
 jwt = JWTManager()
 bcrypt = Bcrypt()
@@ -19,12 +22,17 @@ def create_app(config_class="config.DevelopmentConfig"):
     jwt.init_app(app)
     bcrypt.init_app(app)
 
+    app.config.from_object(config_class)
+
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
-
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
