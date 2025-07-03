d2c959fb91d6cf7449711bb35a5eaa61e14639b1
Sweetyamnesia <angela.r@live.fr>
2025-07-03 14:32:47 +0200

    ajout de is_admin dans user model

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -18,7 +18,8 @@ user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user'),
-    'password': fields.String(required=True, description='Password of the user')
+    'password': fields.String(required=True, description='Password of the user'),
+    'is_admin': fields.Boolean(required=False, description='Set to true to create an admin user', default=False)
 })
 
 @api.route('/')
