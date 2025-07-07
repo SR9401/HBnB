cb0f6b2d4034bf7c824400421d23a9eef8a3bf08
Sweetyamnesia <angela.r@live.fr>
2025-07-07 13:19:26 +0200

    ajout de SQL Alchemy

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
@@ -11,6 +12,7 @@ from app.api.v1.auth import api as auth_ns
 
 jwt = JWTManager()
 bcrypt = Bcrypt()
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -18,6 +20,7 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     jwt.init_app(app)
     bcrypt.init_app(app)
+    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
