952e23ec6e5f8516eab9fa35c05887f0ed0cd615
Sweetyamnesia <angela.r@live.fr>
2025-07-08 11:51:13 +0200

    modification...

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,8 +1,9 @@
 from .basemodel import BaseModel
 import re
 from app import db
-class User(BaseModel):
+class User(db.Model):
     __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(120), unique=True)
