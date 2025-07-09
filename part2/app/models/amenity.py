022c7bb4abfc4deb75551a8f43b93d94844f13e3
Sweetyamnesia <angela.r@live.fr>
2025-07-09 20:22:53 +0200

    rajout du mot validate devant name

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -8,7 +8,7 @@ class Amenity(BaseClass):
     name = db.Column(db.String(50), nullable=False)
 
     @validates('name')
-    def name(self, value):
+    def validate_name(self, value):
         if not isinstance(value, str):
             raise TypeError("Name must be a string")
         if not value:
