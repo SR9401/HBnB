765fc9156e65910fa93a139c9c721c78da2e7f1b
Sweetyamnesia <angela.r@live.fr>
2025-07-10 10:46:50 +0200

    modification de place_id, ajout de la relation place et modification de place_id et user_id dans to dict()

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -2,47 +2,54 @@ from .baseclass import BaseClass
 from app import db
 from .place import Place
 from .user import User
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 import uuid
 
 class Review(BaseClass):
-	__tablename__ = 'reviews'
-	id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-	text = db.Column(db.String(255), nullable=False)
-	rating = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'reviews'
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    text = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
 
-	@validates('text')
-	def validate_text(self, key, value):
-		if not value:
-			raise ValueError("Text cannot be empty")
-		if not isinstance(value, str):
-			raise TypeError("Text must be a string")
-		return value
+    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
 
-	@validates('rating')
-	def validate_rating(self, key, value):
-		if not isinstance(value, int):
-			raise TypeError("Rating must be an integer")
-		super().is_between('Rating', value, 1, 6)
-		return value
+    place = db.relationship(
+        'Place',
+        back_populates='reviews'
+    )
 
-	@validates('place')
-	def validate_place(self, key, value):
-		if not isinstance(value, Place):
-			raise TypeError("Place must be a place instance")
-		return value
+    @validates('text')
+    def validate_text(self, key, value):
+        if not value:
+            raise ValueError("Text cannot be empty")
+        if not isinstance(value, str):
+            raise TypeError("Text must be a string")
+        return value
 
-	@validates('user')
-	def validate_user(self, key, value):
-		if not isinstance(value, User):
-			raise TypeError("User must be a user instance")
-		return value
+    @validates('rating')
+    def validate_rating(self, key, value):
+        if not isinstance(value, int):
+            raise TypeError("Rating must be an integer")
+        super().is_between('Rating', value, 1, 6)
+        return value
 
-	def to_dict(self):
-		return {
-			'id': self.id,
-			'text': self.text,
-			'rating': self.rating,
-			'place_id': self.place.id,
-			'user_id': self.user.id
-		}
+    @validates('place')
+    def validate_place(self, key, value):
+        if not isinstance(value, Place):
+            raise TypeError("Place must be a place instance")
+        return value
+
+    @validates('user')
+    def validate_user(self, key, value):
+        if not isinstance(value, User):
+            raise TypeError("User must be a user instance")
+        return value
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'text': self.text,
+            'rating': self.rating,
+            'place_id': self.place_id,
+            'user_id': self.user_id
+        }
