493a30aa4d899eafa82bfd0f5ce7f66a7ce66b22
SR9401 <shakibrojas@icloud.com>
2025-07-11 15:02:34 +0200

    ajout table user revie et insertion user

--- part3/schema.sql
+++ part3/schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE "user" (
+    id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
 CREATE TABLE place (
     id CHAR(36) PRIMARY KEY,
     title VARCHAR(255),
@@ -7,6 +16,17 @@ CREATE TABLE place (
     longitude FLOAT,
     owner_id CHAR(36),
     FOREIGN KEY (owner_id) REFERENCES "user"(id) ON DELETE CASCADE
+);
+
+CREATE TABLE review (
+    id CHAR(36) PRIMARY KEY,
+    text TEXT,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    user_id CHAR(36),
+    place_id CHAR(36),
+    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES place(id) ON DELETE CASCADE,
+    UNIQUE (user_id, place_id)
 );
 
 CREATE TABLE amenity (
@@ -20,6 +40,16 @@ CREATE TABLE place_amenity (
     PRIMARY KEY (place_id, amenity_id),
     FOREIGN KEY (place_id) REFERENCES place(id) ON DELETE CASCADE,
     FOREIGN KEY (amenity_id) REFERENCES amenity(id) ON DELETE CASCADE
+);
+
+INSERT INTO "user" (id, email, first_name, last_name, password, is_admin)
+VALUES (
+  '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+  'admin@hbnb.io',
+  'Admin',
+  'HBnB',
+  '$2b$12$3pccxczpKkzLTtczk7FJYe5w3/Boe7GV0mUHdLMDoqMNIMfOVhr8e',  -- Mot de passe hashé
+  TRUE
 );
 
 INSERT INTO amenity (id, name) VALUES ("0791af83-2255-49a0-bc55-451ebdf33db8", "Wifi"), ("7883f49c-b4de-4b54-a24f-516ad47aa383", "Piscine"), ("74990c19-313a-4e34-a79d-e18bbd8733e1", "Climatisation");
